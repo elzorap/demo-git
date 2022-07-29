6de36a1ecb916204f473eb4b835ac7cbe6059c11
Mihai Ionescu <ionescumyhay@yahoo.com>
2022-07-29 16:51:32 +0300

    Add new text

---
+++ functions.py
@@ -0,0 +1,13 @@
+def cleanup(string):
+    """
+    Sterge spatiile de la inceput si sfarsit
+    Transforma stringul in formatul prima litera mare, urmatoarele mic
+    Returneaza stringul
+    """
+    new_string = string.strip()
+    new_string = new_string.capitalize()
+    return new_string
+
+
+print(cleanup("  text de transformat"))
+print(cleanup("teST"))
